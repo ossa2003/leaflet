8a3c84991fbfbe67f0ba6e747cc32c963b5cd2c9
ossa2003 <ossa3002@gmail.com>
2023-09-25 10:52:59 +0700

    Base Project Ionic Leaflet

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,46 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  map!: L.Map;
+  baseMaps!: any;
 
+  constructor() {}
+  ngOnInit() {
+  }
+
+  ionViewDidEnter() {
+    this.map = L.map('mapId').setView([35.76943, -580081], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(this.map);
+
+  this.baseMaps = {
+    'OpenStreetMap': L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }),
+    'Topo': L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://opentopomap.org/about" target="_blank">OpenTopoMap</a> contributors',
+    }),
+    'Satellite': L.tileLayer('https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
+      maxZoom: 20,
+      subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+      attribution: '&copy; <a href="https://maps.google.com/">Google Maps</a>'
+    })
+  };
+
+  this.baseMaps['OpenStreetMap'].addTo(this.map);
+
+    L.control.layers(this.baseMaps).addTo(this.map);
+  
+  // Menambahkan marker
+  const marker = new L.Marker([35.76943, -580081]);
+  
+  // Menambahkan marker ke map
+  this.map.addLayer(marker);
+  
+  // Menambahkan Pop up
+  marker.bindPopup('Ini Pop up Ossa');
+  
+  }
 }
